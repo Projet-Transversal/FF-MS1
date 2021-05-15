270ebaa39f680b458f116f030d6aea27e5330889
mehdi-pixel <mehdi050998@gmail.com>
2021-05-15 14:55:57 +0200

    Communication Unidirectionnelle 1.0

--- FF-MS1 MASTER/FF-MS1.c
+++ FF-MS1 MASTER/FF-MS1.c
@@ -13,6 +13,7 @@
 #include "FF-MS1_Config.h"
 #include "FF-MS1_Divers.h"
 #include <stdlib.h>
+#include <string.h>
 
 #define Reset_Timer3Overflow TMR3CN &= 0x04
 #define Disable_Timer3 TMR3CN = 0x00
@@ -20,10 +21,16 @@
 sbit LED = P1^6;
 sbit Button = P3^7;
 sbit SLAVE_NSS = P0^7;
-int i = 0;
-int SPIcounter = 0;
-int SPIlength = 0;
-char xdata buffer[10];
+char i = 0;
+char mCounter = 0;
+char SPIcounter = 0;
+char SPIlength = 0;
+char xdata message[10];
+char* ptr_message = &message;
+char xdata sendBuffer[14];
+char* ptr_sendBuffer = &sendBuffer;
+char xdata recieveBuffer[14];
+char* ptr_recieveBuffer;
 // Prototypes de Fonctions
 
 void Transfert(char c){
@@ -31,6 +38,23 @@ void Transfert(char c){
 	SLAVE_NSS = 0;
 	for(i=0;i < 10;i++);
 	SPI0DAT = c;
+}
+
+void Codage (char SPIlength, char* ptr_sendBuffer, char* ptr_message){
+	*ptr_sendBuffer++ = 'B';
+	*ptr_sendBuffer++ = 'R';
+	while (i < SPIlength){
+		*ptr_sendBuffer++ = *ptr_message++;
+		i++;
+	}
+	while (i < (10)){
+		*ptr_sendBuffer++ = '&';
+		i++;
+	}
+	//TODO : Ajout des caractères vides pour remplir
+	*ptr_sendBuffer++ = 'U';
+	*ptr_sendBuffer++ = 'H';
+	i = 0;
 }
 
 //-----------------------------------------------------------------------------
@@ -56,13 +80,28 @@ void main (void) {
 	
 	while(1)
 	{
-		buffer[0] = 'B';
-		buffer[1] = 'R';
-		buffer[2] = 'E';
-		buffer[3] = 'A';
-		buffer[4] = 'U';
-		buffer[5] = 'H';
-		SPIlength = 2;
+		//Mettre le message à envoyer dans le buffer message, qui n'accepte que 10 char max.
+		message[0] = 'E';
+		message[1] = 'A';
+		SPIlength = 2; //Spécifiez la longueur du message
+		Codage(SPIlength,ptr_sendBuffer,ptr_message);
+		/*
+		
+		if (recieve_flag) {
+			recieve_flag = 0;
+			if (recieveBuffer[1] == 'B' && recieveBuffer[2] == 'R'){
+				while (j != lMessage){
+					messageBuffer[j] = recieveBuffer[j+3];
+					j++;
+					}						
+				if (messageBuffer[0] == 'E' && messageBuffer[1] == 'A'){
+					LED = 1;
+					}
+				}
+		}
+		
+		*/
+		
 	}
 }
 
@@ -85,11 +124,28 @@ void ISR_SPI0() interrupt 6{
 
 void ISR_TMR3() interrupt 14{
 	Reset_Timer3Overflow;
-	if (SPIcounter > SPIlength+3){
+	if (SPIcounter > 13){
 		SPIcounter = 0;
 	}
 	else {
-		Transfert(buffer[SPIcounter]);
+		Transfert(sendBuffer[SPIcounter]);
 		SPIcounter++;
 	}
+	
+	/*
+	//Attente de 2 SCK, pour laisse le temps au Slave d'envoyer son message en retour
+	
+	*ptr_recieveBuffer++ = SPI0DAT;
+	mCounter++;
+	if (*ptr_recieveBuffer == 'H' && *ptr_recieveBuffer == 'U'){
+		lMessage = mCounter-4;
+		mCounter = 0;
+		recieve_flag = 1;
+	}
+	if (i == 10){
+		i = 0;
+	}
+	
+	
+	*/
 }
--- FF-MS1 SLAVE/FF-MS1.c
+++ FF-MS1 SLAVE/FF-MS1.c
@@ -17,14 +17,67 @@
 
 sbit LED = P1^6;
 sbit Button = P3^7;
-char xdata buffer[10];
-char xdata mBuffer[10];
-int i = 0;
-int j = 0;
-int lMessage = 0;
+char xdata messageBuffer[10];
+char xdata message[10];
+char* ptr_message = &message;
+char xdata sendBuffer[14];
+char* ptr_sendBuffer = &sendBuffer;
+char* ogptr_sendBuffer = &sendBuffer;
+char xdata recieveBuffer[14];
+char* ptr_recieveBuffer;
+char mCounter = 0;
+char i = 0;
+char j = 0;
+char k = 0;
+char lMessage = 0;
+char nb_char_vide = 0;
+char SPIlength;
+bit EOR_flag = 0; //End of Reception flag
 bit recieve_flag = 0;
 
 // Prototypes de Fonctions
+
+void Codage(char SPIlength, char* ptr_sendBuffer, char* ptr_message){
+	k=0;
+	if (mCounter == 0){
+		*ptr_sendBuffer++ = 'R';
+		while (k < SPIlength){
+		*ptr_sendBuffer++ = *ptr_message++;
+		k++;
+		}
+		//TODO : Remplir le reste du SendBuffer
+		*ptr_sendBuffer++ = 'U';
+		*ptr_sendBuffer++ = 'H';
+	}
+}
+
+void Envoi(char SPIlength, char* ptr_sendBuffer){
+	mCounter++;
+	if (mCounter == SPIlength + 4){
+		SPI0DAT = 'B';
+		mCounter = 0;
+		ptr_sendBuffer = ogptr_sendBuffer;
+	}
+	else{
+		SPI0DAT = *ptr_sendBuffer++;
+	}
+}
+
+void Decodage(){ //Cette fonction écrit le message reçu dans le messageBuffer
+	if (EOR_flag) {
+		EOR_flag = 0;
+		if (recieveBuffer[0] == 'B' && recieveBuffer[1] == 'R'){
+			while (j != lMessage){
+				messageBuffer[j] = recieveBuffer[j+2];
+				j++;
+				}
+// Vérification du message-test			
+//			if (messageBuffer[0] == 'E' && messageBuffer[1] == 'A'){
+//				LED = 1;
+//				}
+			}
+	}
+}
 
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
@@ -33,6 +86,7 @@ bit recieve_flag = 0;
 void main (void) {
        
 	  Init_Device();  // Appel des configurations globales
+		SPI0DAT = 'B';
 	  
 // Début Insertion Code Configuration des périphériques ***********************
 		EA = 1;
@@ -48,18 +102,17 @@ void main (void) {
 	
 	while(1)
 	{
-		if (recieve_flag) {
-			recieve_flag = 0;
-			if (buffer[1] == 'B' && buffer[2] == 'R'){
-				while (j != lMessage){
-					mBuffer[j] = buffer[j+3];
-					j++;
-					}						
-				if (mBuffer[0] == 'E' && mBuffer[1] == 'A'){
-					LED = 1;
-					}
-				}
-			}
+//		if (recieve_flag){
+//			//On injecte IMMEDIATEMENT une valeur dans le SPI0DAT
+//			// Mettez votre message dans le buffer "message", et indiquez sa longueur
+//			message[0] = 'C';
+//			message[1] = 'J';
+//			SPIlength = 2;
+//			Codage(SPIlength, ptr_sendBuffer, ptr_message);
+//			Envoi(SPIlength,ptr_sendBuffer);
+			//La fonction Envoi garde une idée de l'avancement du message avec un compteur global
+//		}
+			Decodage();
 		}
 	}
 
@@ -77,14 +130,19 @@ void main (void) {
 
 void ISR_SPI0() interrupt 6{
 	SPIF = 0;
-	buffer[i] = SPI0DAT;
+	recieveBuffer[i] = SPI0DAT;
+	recieve_flag = 1;
+	if (recieveBuffer[i] == '&'){
+		nb_char_vide++;
+	}
+	if (recieveBuffer[i] == 'H' && recieveBuffer[i-1] == 'U'){
+		lMessage = i-3-nb_char_vide;
+		i = 0;
+		nb_char_vide = 0;
+		EOR_flag = 1;
+	}
+	if (i == 19){
+		i = 0;
+	}
 	i++;
-	if (buffer[i] == 'H' && buffer[i-1] == 'U'){
-		lMessage = i-4;
-		i = 0;
-		recieve_flag = 1;
-	}
-	if (i == 10){
-		i = 0;
-	}
 }
